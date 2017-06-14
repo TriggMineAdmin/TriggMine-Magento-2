ec23ac2c541355aebfa094642a438a5c725df3d2
Pavel Peshkov <han.paule@gmail.com>
2017-06-14 17:05:32 +0300

    Layout for Fingerprint

---
+++ app/code/Triggmine/Connector/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    map: {
+        '*': {
+            fingerprint2: 'Triggmine_Connector/js/fingerprint2',
+            jscookie: 'Triggmine_Connector/js/jscookie',
+            clientjs: 'Triggmine_Connector/js/client.min'
+        }
+    }
+};
