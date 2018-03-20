b3ba106315ad8fe6fe0b51ff259cb20be4370fae
Pavel Peshkov <han.paule@gmail.com>
2018-03-20 15:17:25 +0200

    change root

--- app/code/Triggmine/Connector/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            fingerprint2: 'Triggmine_Connector/js/fingerprint2',
-            jscookie: 'Triggmine_Connector/js/jscookie',
-            clientjs: 'Triggmine_Connector/js/client.min'
-        }
-    }
-};
